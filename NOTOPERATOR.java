678510c0305d26c5797269a5b32c53ca9d4a62e7
[200~rohitpatil5729~ <rohitpatil5729@gmail.com>
2026-03-19 21:00:00 +0530

    classone

---
+++ NOTOPERATOR.java
@@ -0,0 +1,30 @@
+package Practice;
+
+public class NOTOPERATOR {
+	int ab;
+	public void abc() {
+		
+	}
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		if (!(a>b))
+		{
+			System.out.println("not operator");
+		}
+		System.out.println(a<<3);
+		System.out.println(20>>2);
+		if(a>b) {
+			System.out.println(a);
+		}
+		else if(a<b)
+		{
+			System.out.println(b);
+		}
+		else {
+			System.out.println("ab");
+		}
+		
+	}
+
+}
---
+++ class6.java
@@ -0,0 +1,33 @@
+package Practice;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class class6 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		if(a>b)
+		{
+			System.out.println("a is greater than b");
+		}
+		if(a<b)
+		{
+			System.out.println("b is greater than a");
+		}
+		if(a>b) 
+		{
+			System.out.println("a is greater than b");
+		}
+		else if(a==b)
+		{
+			System.out.println("b is greater than a");
+		}
+		else if(a<b){
+			System.out.println("third condition");
+		}
+		else{
+			
+		}
+	}
+
+}
